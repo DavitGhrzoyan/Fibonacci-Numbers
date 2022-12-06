3302e3143f77865fa07d986c5548e7d53c4bda8a
Davit <ghrzoyandavit@gmail.com>
2022-12-07 01:27:43 +0400

    Add project files.

---
+++ Fibonacci Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fibonacci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Fibonacci(0, 1, 1, number);
+        }
+
+        public static void Fibonacci(int a, int b, int counter, int number)
+        {
+            Console.WriteLine(a);
+            if (counter < number) Fibonacci(b, a + b, counter + 1, number);
+        }
+    }
+}
